fb6c7488dc59a98c2c49ab5c973f59ea1006a2a5
Joe <rxyjoey@gmail.com>
2019-09-12 17:04:44 +0800

    tmp: logrus to fmt

--- keepalive/keepalive.go
+++ keepalive/keepalive.go
@@ -76,9 +76,9 @@ func listenKey() {
 	if pauseKey {
 		isStarted = !isStarted
 		if isStarted {
-			log.Info("开始挂机")
+			fmt.Println("开始挂机")
 		} else {
-			log.Info("停止挂机 按F10再次启动挂机")
+			fmt.Println("停止挂机 按F10再次启动挂机")
 		}
 		listenKey()
 	}
@@ -107,21 +107,21 @@ func activate() {
 		log.Error("激活码错误或已过期")
 		activate()
 	}
-	log.Info("已激活 到期时间 ", dateStr)
+	fmt.Println("已激活 到期时间 ", dateStr)
 }
 
 func main() {
 	log.SetFormatter(&log.TextFormatter{})
 	log.SetLevel(log.DebugLevel)
 
-	log.Info("==================使用说明==================")
-	log.Info("1. 进入魔兽 主城找个人少的地方")
-	log.Info("2. 按F10开始/停止挂机")
-	log.Info("3. 看到人物说话 '1'代表挂机开始 '2'代表挂机结束")
-	log.Info()
-	log.Info("防T机制: 模拟真人键盘操作, 不定期做随机移动等动作")
-	log.Info("如何关闭挂机宝: 直接关掉本窗口")
-	log.Info("==================使用说明==================")
+	fmt.Println("==================使用说明==================")
+	fmt.Println("1. 进入魔兽 主城找个人少的地方")
+	fmt.Println("2. 按F10开始/停止挂机")
+	fmt.Println("3. 看到人物说话 '1'代表挂机开始 '2'代表挂机结束")
+	fmt.Println()
+	fmt.Println("防T机制: 模拟真人键盘操作, 不定期做随机移动等动作")
+	fmt.Println("如何关闭挂机宝: 直接关掉本窗口")
+	fmt.Println("==================使用说明==================")
 	// 验证激活
 	activate()
 	// 监听快捷键
@@ -129,7 +129,7 @@ func main() {
 	// 主循环
 	for {
 		if isStarted {
-			log.Info("5秒后开始模拟操作 请打开魔兽窗口")
+			fmt.Println("5秒后开始模拟操作 请打开魔兽窗口")
 			wowPrintStart()
 			robotgo.Sleep(5)
 			// 挂机动作循环
